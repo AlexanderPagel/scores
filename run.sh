c47e17c7fa93f020efeeab78893212e5ed446793
Alexander Pagel <Alexander.Pagel@ruhr-uni-bochum.de>
2024-06-28 22:22:39 +0200

    command_muse

--- run.sh
+++ run.sh
@@ -33,13 +33,13 @@ load_version "$dotfiles/scripts/version.sh" "0.0.0";
 #load_version "$dotfiles/scripts/bash_meta.sh";
 #load_version "$dotfiles/scripts/cache.sh";
 #load_version "$dotfiles/scripts/error_handling.sh";
-#load_version "$dotfiles/scripts/fileinteracts.sh";
+load_version "$dotfiles/scripts/fileinteracts.sh" "0.0.0";
 #load_version "$dotfiles/scripts/git_utils.sh";
 #load_version "$dotfiles/scripts/progress_bar.sh";
 #load_version "$dotfiles/scripts/setargs.sh";
 #load_version "$dotfiles/scripts/termcap.sh";
-#load_version "$dotfiles/scripts/userinteracts.sh";
-#load_version "$dotfiles/scripts/utils.sh";
+load_version "$dotfiles/scripts/userinteracts.sh" "0.0.0";
+load_version "$dotfiles/scripts/utils.sh" "0.0.0";
 # ╭──────────────────────╮
 # │ 🗺 Globals           │
 # ╰──────────────────────╯
@@ -50,16 +50,39 @@ load_version "$dotfiles/scripts/version.sh" "0.0.0";
 # Default command (when no arguments are given)
 command_default()
 {
-  subcommand move_files;
+  subcommand muse --confirm;
+}
+
+command_muse()
+{
+  set_args "--confirm --help" "$@";
+  eval "$get_args";
+
+  echou "$0 help";
+  echou "$0 COMMAND --help";
+
+  if [[ "$confirm" == "true" ]]; then
+    declare choice="n";
+    boolean_prompt "Run MuseScore?" choice;
+    if [[ "$choice" == "n" ]]; then
+      return 0;
+    fi
+  fi
+  ./MuseScore-Studio-4.3.2.241630832-x86_64.AppImage &
+  :;
 }
 
 command_move_files()
 {
-  set_args "--help";
+  set_args "--help" "$@";
   eval "$get_args";
 
-  mv -v *".mp3" mp3/;
-  mv -v *".pdf" pdf/;
+  ensure_directory mp3;
+  ensure_directory pdf;
+  ensure_directory mscz;
+  2>/dev/null mv -v *".mp3" mp3/ || echos "No mp3 files";
+  2>/dev/null mv -v *".pdf" pdf/ || echos "No pdf files";
+  2>/dev/null mv -v *".mscz" mscz/ || echos "No mscz files";
   echok "Moved files";
 }
 
@@ -72,6 +95,9 @@ command_move_files()
 # ╭──────────────────────╮
 # │ 🖹 Help strings       │
 # ╰──────────────────────╯
+declare -r muse_help_string='Run MuseScore
+OPTIONS
+  --confirm: Confirm before running';
 declare -r move_files_help_string='Move PDF/MP3 files
 Moves files into the subdirectories "pdf/" and "mp3/"';
 # ╭──────────────────────╮
